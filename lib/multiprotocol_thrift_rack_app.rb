c5a34e8bae3de8fae55786f8d2a922e92695a4f4
Dmitrij Fedorenko <c0va23@gmail.com>
2018-02-04 21:27:12 +0300

    Implement multiprotocol thrift server

--- lib/multiprotocol_thrift_rack_app.rb
+++ lib/multiprotocol_thrift_rack_app.rb
@@ -1,3 +1,45 @@
+require 'rack'
+require 'thrift'
+
+# Multiprotocol Thrift Rack application
 class MultiprotocolThriftRackApp
-  # Your code goes here...
+  def initialize(processor, protocol_factory_map)
+    @processor = processor
+    @protocol_factory_map = protocol_factory_map.freeze
+  end
+
+  def call(env)
+    request = Rack::Request.new(env)
+    return failure_response unless request.post?
+    protocol_factory, content_type = find_protocol_factory(request)
+    return failure_response if protocol_factory.nil?
+    successful_response(request.body, protocol_factory, content_type)
+  end
+
+  private
+
+  def failure_response
+    Rack::Response.new([], 400, {})
+  end
+
+  def find_protocol_factory(request)
+    content_type = request.get_header(Rack::CONTENT_TYPE)
+    @protocol_factory_map.each do |(protocol_factory, content_types)|
+      next unless content_types.include?(content_type)
+      return protocol_factory, content_type
+    end
+    nil
+  end
+
+  def successful_response(request_body, protocol_factory, content_type)
+    Rack::Response.new(
+      [],
+      200,
+      Rack::CONTENT_TYPE => content_type,
+    ) do |response|
+      transport = Thrift::IOStreamTransport.new(request_body, response)
+      protocol = protocol_factory.get_protocol(transport)
+      @processor.process(protocol, protocol)
+    end
+  end
 end
--- spec/multiprotocol_thrift_rack_app_spec.rb
+++ spec/multiprotocol_thrift_rack_app_spec.rb
@@ -1,7 +1,92 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe MultiprotocolThriftRackApp do
-  it 'simple test' do
-    expect(true).to eq false
+  describe '#call' do
+    subject(:response) { rack_app.call(request_env) }
+
+    let(:processor) { instance_double('Thrift::Processor') }
+
+    let(:binary_protocol_factory) do
+      instance_double('Thrift::BinaryProtocolFactory')
+    end
+    let(:json_protocol_factory) do
+      nstance_double('Thrift::JsonProtocolFactory')
+    end
+
+    let(:protocol_factory_map) do
+      {
+        binary_protocol_factory => ['application/x-thrift'],
+        json_protocol_factory => ['application/json'],
+      }
+    end
+
+    let(:rack_app) { described_class.new(processor, protocol_factory_map) }
+
+    let(:request_method) { Rack::POST }
+    let(:request_body) { StringIO.new('custom request body') }
+    let(:request_env) do
+      {
+        Rack::REQUEST_METHOD => request_method,
+        Rack::CONTENT_TYPE => content_type,
+        Rack::RACK_INPUT => request_body,
+      }
+    end
+
+    context 'when request with custom content type' do
+      let(:content_type) { 'text/palin' }
+
+      it 'return response with status 400' do
+        expect(response.status).to eq 400
+      end
+    end
+
+    shared_examples 'process POST request' do
+      context 'when reqeust with POST method' do
+        let(:request_method) { Rack::POST }
+
+        before do
+          transport = instance_double('Transport')
+          protocol = instance_double('Thrift::BaseProtocol')
+
+          allow(Thrift::IOStreamTransport).to receive(:new)
+            .with(request_body, kind_of(Rack::Response))
+            .and_return(transport)
+          allow(protocol_factory).to receive(:get_protocol)
+            .with(transport)
+            .and_return(protocol)
+          allow(processor).to receive(:process).with(protocol, protocol)
+        end
+
+        it 'return response with status 200' do
+          expect(response.status).to eq 200
+        end
+
+        it 'return valid content type header' do
+          expect(response.headers[Rack::CONTENT_TYPE]).to eq content_type
+        end
+      end
+
+      context 'when request with GET method' do
+        let(:request_method) { Rack::GET }
+
+        it 'return response with status 400' do
+          expect(response.status).to eq 400
+        end
+      end
+    end
+
+    context 'when request with first content type' do
+      let(:content_type) { 'application/x-thrift' }
+      let(:protocol_factory) { binary_protocol_factory }
+
+      include_examples 'process POST request'
+    end
+
+    context 'when request with second content type' do
+      let(:content_type) { 'application/json' }
+      let(:protocol_factory) { json_protocol_factory }
+
+      include_examples 'process POST request'
+    end
   end
 end
